3c053b793cb35d5b3329cf8608eb0d4b476b9817
jiaoyuan <quic_jiaoyuan@quicinc.com>
2024-08-07 11:55:20 +0530

    Add C2PA resource files and parsing files
    Change-Id: Ieb850552789b26caf49da8beafb4d745d8da47bf
    CRs-Fixed:3879832
    (cherry picked from commit 520c561852de1d5bc142ef10fc7b8750d314a032)

---
+++ src/com/truepic/lensverify/data/c2padata/assertions/actions/C2PAActionDataActions.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2024 Truepic
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.truepic.lensverify.data.c2padata.assertions.actions;
+
+import java.util.Map;
+
+public class C2PAActionDataActions {
+
+    private String action;
+    private String softwareAgent;
+    private String when;
+    private Map<String, String> parameters;
+
+    public String getAction() {
+        return action;
+    }
+
+    public String getSoftwareAgent() {
+        return softwareAgent;
+    }
+
+    public String getWhen() {
+        return when;
+    }
+
+    public Map<String, String> getParameters() {
+        return parameters;
+    }
+}
